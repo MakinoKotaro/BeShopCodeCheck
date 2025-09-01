3ba1800d3d2ed2f7b579ab7bbde67ef5d2aa7493
MakinoKotaro <167941540+MakinoKotaro@users.noreply.github.com>
2025-09-01 13:33:41 +0900

    Add files via upload

---
+++ ScriptableObject/ShortageMoney.cs
@@ -0,0 +1,74 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+/// <summary>
+/// 所持金不足時のアニメーション付きUI表示を行うクラス
+/// </summary>
+public class ShortageMoney : MonoBehaviour
+{
+    #region 変数宣言
+    [SerializeField] private TextMeshProUGUI shortageMoneyText; // 不足金額のテキスト
+
+    private SFXManager sfxManager;       // 効果音再生用マネージャ
+    #endregion
+
+    #region Unityイベント
+    private void Start()
+    {
+        shortageMoneyText.enabled = false;                         // 初期状態では非表示
+        shortageMoneyText.transform.localScale = Vector3.one * 2f; // 初期スケール（2倍）
+        shortageMoneyText.alpha = 0f;                               // 初期透明
+
+        sfxManager = FindAnyObjectByType<SFXManager>();
+    }
+    #endregion
+
+    #region カスタムメソッド
+    /// <summary>
+    /// 所持金不足の演出を表示する
+    /// </summary>
+    public void PopShortageMoneyText()
+    {
+        shortageMoneyText.enabled = true;
+        shortageMoneyText.alpha = 1f; // 表示（不透明）
+
+        Sequence animationSequence = DOTween.Sequence();
+
+        animationSequence
+            .Append(
+                shortageMoneyText.transform
+                    .DOScale(Vector3.one, 0.5f) // 通常サイズへ
+                    .SetEase(Ease.OutBack)
+            )
+            .Join(
+                shortageMoneyText.transform
+                    .DORotate(new Vector3(0, 0, 360), 0.5f, RotateMode.FastBeyond360) // 一回転
+                    .SetEase(Ease.OutCubic)
+                    .OnComplete(() =>
+                    {
+                        sfxManager.OnClickBackButton();
+                    })
+            )
+            .AppendInterval(1f) // 表示維持時間
+            .Append(
+                shortageMoneyText
+                    .DOFade(0f, 0.5f)
+                    .SetEase(Ease.InCubic)
+            )
+            .OnComplete(ResetText);
+    }
+
+    /// <summary>
+    /// テキスト表示を初期状態に戻す
+    /// </summary>
+    private void ResetText()
+    {
+        shortageMoneyText.enabled = false;
+        shortageMoneyText.transform.localScale = Vector3.one * 2f;
+        shortageMoneyText.transform.rotation = Quaternion.identity;
+        shortageMoneyText.alpha = 0f;
+    }
+    #endregion
+}
---
+++ ScriptableObject/ShowDescription.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class ShowDescription : MonoBehaviour
+{
+    [SerializeField] private ShopUiManager shopUiManager;
+    [SerializeField] private PurchaseManager purchaseManager;
+
+    [SerializeField] private GameObject[] itemImages = new GameObject[8];
+
+    [SerializeField] SO_ShopItem[] shopItems = new SO_ShopItem[8];
+
+    //private void OnMouseEnter()
+    //{
+    //    Debug.Log("DWAD");
+    //    Debug.Log("Mouse entered: " + gameObject.name);
+
+    //    switch (gameObject.tag)
+    //    {
+    //        case "ShopItem1":
+    //            shopUiManager.ShowDescUi(shopItems[0]);
+    //            break;
+    //        case "ShopItem2":
+    //            shopUiManager.ShowDescUi(shopItems[1]);
+    //            break;
+    //        case "ShopItem3":
+    //            shopUiManager.ShowDescUi(shopItems[2]);
+    //            break;
+    //        case "ShopItem4":
+    //            shopUiManager.ShowDescUi(shopItems[3]);
+    //            break;
+    //        case "ShopItem5":
+    //            shopUiManager.ShowDescUi(shopItems[4]);
+    //            break;
+    //        case "ShopItem6":
+    //            shopUiManager.ShowDescUi(shopItems[5]);
+    //            break;
+    //        case "ShopItem7":
+    //            shopUiManager.ShowDescUi(shopItems[6]);
+    //            break;
+    //        case "ShopItem8":
+    //            shopUiManager.ShowDescUi(shopItems[7]);
+    //            break;
+    //        default:
+
+    //            break;
+    //    }
+    //}
+
+    //private void OnMouseExit()
+    //{
+    //    shopUiManager.HideDescUi();
+    //}
+
+}
